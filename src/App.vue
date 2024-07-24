f2af765a0e647266b371a32cd886031b40ed34f2
AntenaMarques <maicon.marques@atlastechnol.com>
2024-07-24 12:12:07 -0300

    feat: add files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
